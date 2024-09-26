7bfe420dfa1a27c22f1ccaf5d9d23fc9f74e5de3
mateuscastronappe <teteucastro31@gmail.com>
2024-09-26 11:33:59 -0300

    Tailwind explanado com plugins forms e typography

--- aula14/src/components/Rodape/Rodape.tsx
+++ aula14/src/components/Rodape/Rodape.tsx
@@ -2,8 +2,8 @@
 export default function Rodape() {
     return (
         <footer>
-            <h2>Rodape</h2>
-            <p>Desenvolvido por <span>pf765</span></p>
+            <h2 className="bg-background text-foreground">Rodape</h2>
+            <p>Desenvolvido por <span>rm554661</span></p>
         </footer>
     )
 }
