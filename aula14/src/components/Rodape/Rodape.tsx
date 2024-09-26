e4d033430d9bffad1a7b8ea9a2f119659e7a1342
jenniesuzuki <jenniferkaorisuzuki@gmail.com>
2024-09-26 11:28:34 -0300

    Tailwind explanado com plugins forms e typography

--- aula14/src/components/Rodape/Rodape.tsx
+++ aula14/src/components/Rodape/Rodape.tsx
@@ -2,7 +2,7 @@
 export default function Rodape() {
     return (
         <footer>
-            <h2>Rodape</h2>
+            <h2 className="bg-background text-foreground">Rodape</h2>
             <p>Desenvolvido por <span>rm554661</span></p>
         </footer>
     )
