2bb636553a9cd2a944c72bb2bfc69c78a213b32b
victoraugustogfavaro <victoraugustogfavaro@gmail.com>
2024-09-26 11:29:42 -0300

    Tailwind

--- aula14/src/app/layout.tsx
+++ aula14/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Cabecalho from "@/components/Cabecalho/Cabecalho";
 import Rodape from "@/components/Rodape/Rodape";
 import type { Metadata } from "next";
+import "@/app/globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -10,8 +11,8 @@ export const metadata: Metadata = {
 export const viewport = {
   initialScale: 1.0,
   width: "device-width",
-  colorScheme: "light"
-}
+  colorScheme: "light",
+};
 
 export default function RootLayout({
   children,
